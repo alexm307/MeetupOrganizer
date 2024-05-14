e90976f66cb5c04c4de04d4503dc719d6ce82652
Muresan Ioan-Alexandru <58291370+alexm307@users.noreply.github.com>
2024-05-15 00:09:29 +0300

    finished functionality

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.hello)
+    path('meetups/', views.hello, name='hello'),
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup_details')
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -23,3 +23,15 @@ def hello(request):
     return render(request, 'meetups/index.html', {
         'meetups': meetups
     })
+
+
+def meetup_details(request, meetup_slug):
+    selected_meetup = {
+        'title': 'Second Meetup',
+        'location': 'Sibiu',
+        'slug': 'second-meetup'
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['location']
+    })
